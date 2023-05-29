1b329ea789ff1bad548c6915a84fce7f046efaeb
David Nguyen <davido.nguyen1212@gmail.com>
2023-05-29 12:45:46 -0700

    final commit of test repo

--- hello.py
+++ hello.py
@@ -4,3 +4,16 @@ print("Venushack 2023")
 print('huan is a baddie')
 
 # comments are not done using //
+
+# x = [[1, 2, 3]
+#  [4, 5, 6]
+#  [7, 8, 9]
+# ]
+#
+# i = 0 ->[1, 2, 3]
+# i = 1 ->[4, 5, 6]
+# i = 2 ->[7, 8, 9]
+
+# for (i = 0; )
+
+print(4 + 6 % 12 / 4)
